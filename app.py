1ec1a6f3c67a6ce9a54667f0c72cd3b596fdd372
Adriana A Rojas <aarojas@Adrianas-MacBook-Pro.local>
2020-06-08 00:34:00 -0400

    Update app.py to include model and fix paths to sample audio files

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import numpy as np
-import librosa 
-esc50path="/Users/aarojas/Documents/Data_Science_Resources/Insight_20B/AAR_Insight_Project/Firearm_Detection/data/raw/ESC-50-master/audio/"
-fapath="/Users/aarojas/Documents/Data_Science_Resources/Insight_20B/AAR_Insight_Project/Firearm_Detection/data/raw/Firearms/"
+import librosa
+import pickle
+
+# load the model from disk
+model_path="models/"
+log_res_model="logistic_regression_model.sav"
+loaded_model = pickle.load(open(model_path+log_res_model, 'rb'))
+#result = loaded_model.score(X_test, Y_test)
+#print(result)
+
+path="data/external/"
 audio_clip1='5-160614-H-48.wav' #fireworks
 audio_clip2='5-244526-A-26.wav' #laughing
 audio_clip3='385785__rijam__rifle-shot-m1-garand.mp3'
@@ -13,16 +21,16 @@ st.header('Listening for Firearms in Your Home')
 st.text('The following are a set of sample audio clips that can be input into the model.')
 
 
-st.audio(esc50path+audio_clip1)
+st.audio(path+audio_clip1)
 
 
 st.text('This is audio clip 1.')
 
-st.audio(esc50path+audio_clip2)
+st.audio(path+audio_clip2)
 
 st.text('This is audio clip 2.')
 
-st.audio(fapath+audio_clip3)
+st.audio(path+audio_clip3)
 
 option = st.selectbox('Select the clip you would like the model to analyze.',('Audio clip 1', 'Audio clip 2', 'Audio clip 3'))
 st.write('You selected:', option)
