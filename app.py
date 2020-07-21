83c8e0ede624c4f1ccd0df5e02ac1eebe2f5675b
Adriana A Rojas <aarojas@Adrianas-MacBook-Pro.local>
2020-07-21 14:31:15 -0400

    Add radio buttons to app for organization; add updated CNN model

--- app.py
+++ app.py
@@ -1,53 +1,11 @@
 import streamlit as st
-#import argparse
 import numpy as np
 from tensorflow.keras.models import load_model
 import librosa
 import time
 
-#import sounddevice as sd
-#import soundfile as sf
 import matplotlib.pyplot as plt
-##
-# def int_or_str(text):
-#     """Helper function for argument parsing."""
-#     try:
-#         return int(text)
-#     except ValueError:
-#         return text
-#
-#
-# parser = argparse.ArgumentParser(add_help=False)
-# parser.add_argument(
-#     '-l', '--list-devices', action='store_true',
-#     help='show list of audio devices and exit')
-# args, remaining = parser.parse_known_args()
-# if args.list_devices:
-#     print(sd.query_devices())
-#     parser.exit(0)
-# parser = argparse.ArgumentParser(
-#     description=__doc__,
-#     formatter_class=argparse.RawDescriptionHelpFormatter,
-#     parents=[parser])
-# parser.add_argument(
-#     'filename', nargs='?', metavar='FILENAME',
-#     help='audio file to store recording to')
-# parser.add_argument(
-#     '-d', '--device', type=int_or_str,
-#     help='input device (numeric ID or substring)')
-# parser.add_argument(
-#     '-r', '--samplerate', type=int, help='sampling rate')
-# parser.add_argument(
-#     '-c', '--channels', type=int, default=1, help='number of input channels')
-# parser.add_argument(
-#     '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
-# args = parser.parse_args(remaining)
-#
-#
-# if args.samplerate is None:
-#     device_info = sd.query_devices(args.device, 'input')
-#     # soundfile expects an int, sounddevice provides a float:
-#     args.samplerate = int(device_info['default_samplerate'])
+
 
 def wav2mfcc(wave, sr=22050,n_mfcc=20, max_len=170):
     '''wave is a np array'''
@@ -65,10 +23,18 @@ def wav2mfcc(wave, sr=22050,n_mfcc=20, max_len=170):
 
     return mfcc
 
+def updateplot(wave,txt_output):
+    """
+    update the plot with the wave file
+    """
+
+    line.set_ydata(wave)
+    the_plot.pyplot(plt)
+    text.set_text(txt_output)
 
 # load the model from disk
 model_path="models/"
-cnn_model=load_model(model_path+'cnn_model_accuracy_92.0_alpha_0.0002.h5')
+cnn_model=load_model(model_path+'bal_cnn_model_accuracy_98.2_alpha_0.0001.h5')
 
 #-------------------------------------------------
 
@@ -84,161 +50,95 @@ audio_dict={
 'Audio clip 1':audio_clip1,
 'Audio clip 2': audio_clip2,
 'Audio clip 3': audio_clip3}
+#-----------------------------------------------
+# select a sidebar to navigate between different options of the app
+options=['Test with some sample clips', 'Test with a youtube video']
+page=st.sidebar.radio('Select an option',options)
 
-st.text('The following are a set of sample audio clips that can be input into the model.')
+st.sidebar.header('Firearm-Alarm Options')
+st.sidebar.markdown('The first option will allow you to test firearm-alarm with some pre-recorded sound clips.')
+st.sidebar.markdown('The second option will enable you to have firearm-alarm listen to a youtube clip: https://www.youtube.com/watch?v=1N_m3tsPyP0.')
+#-----------------------------------------------
+if page==options[0]: #The first option is selected
+
+    st.text('The following are a set of sample audio clips that can be input into the model.')
 
 
-st.audio(path+audio_clip1)
+    st.audio(path+audio_clip1)
 
 
-st.text('This is audio clip 1.')
+    st.text('This is audio clip 1.')
 
-st.audio(path+audio_clip2)
+    st.audio(path+audio_clip2)
 
-st.text('This is audio clip 2.')
+    st.text('This is audio clip 2.')
 
-st.audio(path+audio_clip3)
+    st.audio(path+audio_clip3)
 
-option = st.selectbox('Select the clip you would like the model to analyze.',('Audio clip 1', 'Audio clip 2', 'Audio clip 3'))
-st.write('You selected:', option)
+    option = st.selectbox('Select the clip you would like the model to analyze.',('Audio clip 1', 'Audio clip 2', 'Audio clip 3'))
+    st.write('You selected:', option)
 
-if st.button('Analyze '+option):
-    wave, sr = librosa.load(path+audio_dict[option], mono=True, sr=22050)
-    mfcc=wav2mfcc(wave,sr=sr)
-    X_test = np.reshape(mfcc,(1, 20, 170, 1))
+    if st.button('Analyze '+option):
+        wave, sr = librosa.load(path+audio_dict[option], mono=True, sr=22050)
+        mfcc=wav2mfcc(wave,sr=sr)
+        X_test = np.reshape(mfcc,(1, 20, 170, 1))
 
-    Y_predict=cnn_model.predict(X_test)
-    print(Y_predict)
+        Y_predict=cnn_model.predict(X_test)
+        print(Y_predict)
 
-    if Y_predict.round()[0][0]==0 :
+        if Y_predict.round()[0][0]==1 :
 
-        st.write("This doesn't sound like a firearm.")
+            st.write("This doesn't sound like a firearm.")
 
-    if Y_predict.round()[0][0]==1:
+        if Y_predict.round()[0][0]==0:
 
-        st.write("This is a firearm! Contacting local authorities...")
+            st.write("This is a firearm! Contacting local authorities...")
 
-else:
-    st.write('Click the button to analyze the audio clip.')
+    else:
+        st.write('Click the button to analyze the audio clip.')
 
 ###############################################----------------------------------
+elif page==options[1]: #if the second page is selected
 
-st.header('Firearm Alarm in Action')
-st.text('For how long would you like Firearm Alarm to listen?')
-t=st.slider('Select the time (hours).',min_value=1,max_value=24,step=1)
+    st.header('Firearm Alarm in Action')
 
-x = np.arange(0, 4,1/22050)
-fig, ax=plt.subplots()
-ax.set_ylim(-1, 1)
-line, = ax.plot(x, np.zeros(len(x)),color='m',linewidth=2)
-plt.xlabel('Time (s)')
-plt.ylabel('Sound Wave')
-the_plot = st.pyplot(plt)
-
-
-def updateplot(wave):
-    """
-    update the plot with the wave file
-    """
-
-    line.set_ydata(wave)
-    the_plot.pyplot(plt)
+    x = np.arange(0, 4,1/22050)
+    fig, ax=plt.subplots()
+    ax.set_ylim(-1, 1)
+    line, = ax.plot(x, np.zeros(len(x)),color='m',linewidth=2)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Sound Wave')
+    the_plot = st.pyplot(plt)
+    text=plt.text(0,.8,'',fontsize=14)
 
 
-sample='data/external/Cooking_clip.wav'
-if st.button('See an example with Firearm Alarm'):
-    with st.spinner("Listening..."):
-        array,sr=librosa.load(sample)
-        tiempo=librosa.get_duration(array) #time in seconds
-        for t in range(0,int(tiempo),4):
-            wave, sr = librosa.load(sample, mono=True,offset=t,duration=4)
+    sample='data/external/Real_life_gunshot_sound_effects.mp3'
+    if st.button('See an example with Firearm Alarm'):
+        with st.spinner("Listening..."):
+            array,sr=librosa.load(sample)
+            tiempo=librosa.get_duration(array) #time in seconds
+            for t in range(0,int(tiempo),4):
+                wave, sr = librosa.load(sample, mono=True,offset=t,duration=4)
 
-            ## run it through the model
-            mfcc=wav2mfcc(wave)
+                ## run it through the model
+                mfcc=wav2mfcc(wave)
 
-            X_test = np.reshape(mfcc,(1, 20, 170, 1))
-            Y_predict=cnn_model.predict(X_test)
+                X_test = np.reshape(mfcc,(1, 20, 170, 1))
+                Y_predict=cnn_model.predict(X_test)
 
-            if Y_predict.round()[0][0]==0 :
-                plt.text(0,.8,'All sounds safe.',fontsize=14,color='slateblue')
-                #st.write("All sounds safe.")
+                if Y_predict.round()[0][0]==1 :
+                    txt_output='No firearm sound(s) detected'
+                    # text.set_text('No firearm sounds detected')
 
-            if Y_predict.round()[0][0]==1:
-                plt.text(.5,.8,'This is a firearm!')
-                #st.write("This is a firearm! Contacting local authorities...")
+                if Y_predict.round()[0][0]==0:
+                    txt_output='Firearm sound(s) detected!'
+                    # text.set_text('Firearm sounds detected!')
 
-            updateplot(wave)
-            time.sleep(3)
+                updateplot(wave,txt_output)
+                time.sleep(3)
 
-            plt.show()
-else:
-    st.write('Click the button to start listening.')
+                plt.show()
+    else:
+        st.write('Click the button to start listening.')
 
-############################################-----------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st.header('Try Firearm Alarm with your own sound clips')
-#
-# uploaded_file = st.file_uploader("Upload a .WAV file", type="wav")
-# if uploaded_file is not None:
-#     uploaded_file=uploaded_file.read().decode("utf-8")
-#     print(type(uploaded_file))
-#     array,sr=librosa.load(uploaded_file)
-#     tiempo=librosa.get_duration(array) #time in seconds
-#
-# x = np.arange(0, 4,1/22050)
-# fig, ax=plt.subplots()
-# ax.set_ylim(-1, 1)
-# line, = ax.plot(x, np.zeros(len(x)),color='royalblue',linewidth=2)
-# plt.xlabel('Time (s)')
-# plt.ylabel('Sound Wave')
-# the_plot = st.pyplot(plt)
-#
-# if uploaded_file is not None:
-#     if st.button('Analyze your sound clip'):
-#         with st.spinner("Listening..."):
-#             array,sr=librosa.load(uploaded_file)
-#             tiempo=librosa.get_duration(array) #time in seconds
-#             for t in range(0,int(tiempo),4):
-#                 wave, sr = librosa.load(uploaded_file, mono=True,offset=t,duration=4)
-#
-#                 ## run it through the model
-#                 mfcc=wav2mfcc(wave)
-#
-#                 X_test = np.reshape(mfcc,(1, 20, 170, 1))
-#                 Y_predict=cnn_model.predict(X_test)
-#
-#                 if Y_predict.round()[0][0]==0 :
-#                     plt.text(0,.8,'All sounds safe.',fontsize=14,color='slateblue')
-#                     #st.write("All sounds safe.")
-#
-#                 if Y_predict.round()[0][0]==1:
-#                     plt.text(.5,.8,'This is a firearm!')
-#                     #st.write("This is a firearm! Contacting local authorities...")
-#
-#                 updateplot(wave)
-#                 time.sleep(3)
-#
-#                 plt.show()
-#     else:
-#         st.write('Click the button to start listening.')
+#-----------------------------------
--- app_demo.py
+++ app_demo.py
@@ -5,7 +5,6 @@ from tensorflow.keras.models import load_model
 import librosa
 
 import sounddevice as sd
-#import soundfile as sf
 import matplotlib.pyplot as plt
 ##
 def int_or_str(text):
@@ -65,10 +64,21 @@ def wav2mfcc(wave, sr=args.samplerate,n_mfcc=20, max_len=170):
 
     return mfcc
 
+def record(sr=args.samplerate, channels=args.channels, duration=4, filename='pred_record.wav'):
+    """
+    Records live environment surroundings
+    """
+    recording = sd.rec(int(duration * sr), samplerate=sr, channels=channels).reshape(-1)
+    sd.wait()
 
-# load the model from disk
+    line.set_ydata(recording)
+    the_plot.pyplot(plt)
+
+    return recording
+
+# load the model
 model_path="models/"
-cnn_model=load_model(model_path+'cnn_model_accuracy_92.0_alpha_0.0002.h5')
+cnn_model=load_model(model_path+'bal_cnn_model_accuracy_97.7_alpha_0.0001.h5')
 
 #-------------------------------------------------
 
@@ -84,91 +94,86 @@ audio_dict={
 'Audio clip 1':audio_clip1,
 'Audio clip 2': audio_clip2,
 'Audio clip 3': audio_clip3}
+#-----------------------------------------------
+# select a sidebar to navigate between different options of the app
+options=['Test with some sample clips', 'Record your sound']
+page=st.sidebar.radio('Select an option',options)
 
-st.text('The following are a set of sample audio clips that can be input into the model.')
+st.sidebar.header('Firearm-Alarm Options')
+st.sidebar.markdown('The first option will allow you to test firearm-alarm with some pre-recorded sound clips.')
+st.sidebar.markdown('The second option will enable you to have firearm-alarm listen to your environment and report its findings.')
+#######
+if page==options[0]: #The first option is selected
+    st.text('The following are a set of sample audio clips that can be input into the model.')
 
 
-st.audio(path+audio_clip1)
+    st.audio(path+audio_clip1)
 
 
-st.text('This is audio clip 1.')
+    st.text('This is audio clip 1.')
 
-st.audio(path+audio_clip2)
+    st.audio(path+audio_clip2)
 
-st.text('This is audio clip 2.')
+    st.text('This is audio clip 2.')
 
-st.audio(path+audio_clip3)
+    st.audio(path+audio_clip3)
 
-option = st.selectbox('Select the clip you would like the model to analyze.',('Audio clip 1', 'Audio clip 2', 'Audio clip 3'))
-st.write('You selected:', option)
+    option = st.selectbox('Select the clip you would like the model to analyze.',('Audio clip 1', 'Audio clip 2', 'Audio clip 3'))
+    st.write('You selected:', option)
 
-if st.button('Analyze '+option):
-    wave, sr = librosa.load(path+audio_dict[option], mono=True, sr=22050)
-    mfcc=wav2mfcc(wave,sr=sr)
-    X_test = np.reshape(mfcc,(1, 20, 170, 1))
+    if st.button('Analyze '+option):
+        wave, sr = librosa.load(path+audio_dict[option], mono=True, sr=22050)
+        mfcc=wav2mfcc(wave,sr=sr)
+        X_test = np.reshape(mfcc,(1, 20, 170, 1))
 
-    Y_predict=cnn_model.predict(X_test)
-    print(Y_predict)
+        Y_predict=cnn_model.predict(X_test)
+        print(Y_predict)
 
-    if Y_predict.round()[0][0]==0 :
+        if Y_predict.round()[0][0]==1 :
 
-        st.write("This doesn't sound like a firearm.")
+            st.write("This doesn't sound like a firearm.")
 
-    if Y_predict.round()[0][0]==1:
+        if Y_predict.round()[0][0]==0:
 
-        st.write("This is a firearm! Contacting local authorities...")
+            st.write("This is a firearm! Contacting local authorities...")
 
-else:
-    st.write('Click the button to analyze the audio clip.')
+    else:
+        st.write('Click the button to analyze the audio clip.')
 
 ###############################################----------------------------------
+elif page==options[1]: #if the second page is selected
+    st.header('Firearm Alarm in Action')
+    st.text('For how long would you like Firearm Alarm to listen?')
+    t=st.slider('Select the time (hours).',min_value=1,max_value=24,step=1)
 
-st.header('Firearm Alarm in Action')
-st.text('For how long would you like Firearm Alarm to listen?')
-t=st.slider('Select the time (hours).',min_value=1,max_value=24,step=1)
+    x = np.arange(0, 4,1/int(args.samplerate))
+    fig, ax=plt.subplots()
+    ax.set_ylim(-1, 1)
+    line, = ax.plot(x, np.zeros(len(x)),color='m',linewidth=2)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Sound Wave')
+    the_plot = st.pyplot(plt)
 
-x = np.arange(0, 4,1/int(args.samplerate))
-fig, ax=plt.subplots()
-ax.set_ylim(-1, 1)
-line, = ax.plot(x, np.zeros(len(x)),color='m',linewidth=2)
-plt.xlabel('Time (s)')
-plt.ylabel('Sound Wave')
-the_plot = st.pyplot(plt)
+    if st.button('Start listening with Firearm Alarm'):
+        with st.spinner("Listening..."):
+            for i in range(0,int(t*3600)):
 
+                recording=record()
 
-def record(sr=args.samplerate, channels=args.channels, duration=4, filename='pred_record.wav'):
-    """
-    Records live voice
-    """
-    recording = sd.rec(int(duration * sr), samplerate=sr, channels=channels).reshape(-1)
-    sd.wait()
+                ## run it through the model
+                mfcc=wav2mfcc(recording)
 
-    line.set_ydata(recording)
-    the_plot.pyplot(plt)
+                X_test = np.reshape(mfcc,(1, 20, 170, 1))
+                Y_predict=cnn_model.predict(X_test)
 
-    return recording
+                if Y_predict.round()[0][0]==1 :
+                    plt.text(0,.8,'All sounds safe.',fontsize=14,color='slateblue')
+                    #st.write("All sounds safe.")
 
+                if Y_predict.round()[0][0]==0:
+                    plt.text(.5,.8,'This is a firearm!')
+                    #st.write("This is a firearm! Contacting local authorities...")
 
-if st.button('Start listening with Firearm Alarm'):
-    with st.spinner("Listening..."):
-        for i in range(0,int(t*3600)):
-
-            recording=record()
-
-            ## run it through the model
-            mfcc=wav2mfcc(recording)
-
-            X_test = np.reshape(mfcc,(1, 20, 170, 1))
-            Y_predict=cnn_model.predict(X_test)
-
-            if Y_predict.round()[0][0]==0 :
-                plt.text(0,.8,'All sounds safe.',fontsize=14,color='slateblue')
-                #st.write("All sounds safe.")
-
-            if Y_predict.round()[0][0]==1:
-                plt.text(.5,.8,'This is a firearm!')
-                #st.write("This is a firearm! Contacting local authorities...")
-
-            plt.show()
-else:
-    st.write('Click the button to start listening.')
+                plt.show()
+    else:
+        st.write('Click the button to start listening.')
